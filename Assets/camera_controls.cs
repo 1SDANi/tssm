0a8c5a9934e0f3724eee10d2966abb10563e38d7
earthcrafterman <banddstudios@gmail.com>
2019-06-10 07:26:27 -0400

    Added player ground movement - Added camera relative movement - Added friction - Added forward movement - Added a magnitude based forward movement modifier - Added turning - Used Slerp for turning instead of Lerp so that turns of higher degrees are faster - Slerp turning and magnitude boosting work to offset each other's viability, so that there's no universal choice for how to turn
    Added a basic camera
    - Added a basic camera implementation
    - Added camera zooming
    - Added camera rotating

---
+++ Assets/camera_controls.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_controls : MonoBehaviour
+{
+    public string mouseY = "Mouse Y";
+    public string mouseX = "Mouse X";
+    public string toggleLock = "Cancel";
+    public string scroll = "Mouse ScrollWheel";
+    public int horizontalMouseSpeed;
+    public int verticalMouseSpeed;
+    public int mouseScrollSpeed;
+
+    public bool lockMouse;
+    public bool lockUnlocked;
+    
+    public float maxDistance;
+    public float minDistance;
+    public float maxVerticalAngle;
+    public float minVerticalAngle;
+
+    public Transform follow;
+    public Vector3 followOffset;
+    public float CameraHeight;
+    public float DistanceOffset { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = follow.position + followOffset;
+        lockMouse = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = follow.position + followOffset;
+        HandleCamera();
+    }
+
+    void HandleCamera()
+    {
+        // Upon pressing the Lock Toggle down, toggle the lock.
+        if (Input.GetAxis(toggleLock) > 0)
+        {
+            if (lockUnlocked)
+            {
+                lockMouse = !lockMouse;
+                lockUnlocked = false;
+            }
+        }
+        else
+        {
+            lockUnlocked = true;
+        }
+
+        // While the mouse is supposed to be locked, lock and hide the mouse and handle camera movement
+        if (lockMouse)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            Look();
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    void Look()
+    {
+        // Get camera movement input from the player
+        Vector3 mouseInput = new Vector3(Input.GetAxis(mouseY), Input.GetAxis(mouseX), Input.GetAxis(scroll));
+        
+        if (mouseInput.x != 0)
+        {
+            // Rotate the camera vertically
+            Camera.main.transform.Rotate(mouseInput.x * verticalMouseSpeed * Time.deltaTime, 0, 0);
+            // Clamp the camera's vertical rotation
+            Vector3 cameraRotation = Camera.main.transform.rotation.eulerAngles;
+            Camera.main.transform.rotation = Quaternion.Euler(Mathf.Clamp(cameraRotation.x, minVerticalAngle, maxVerticalAngle), cameraRotation.y, cameraRotation.z);
+        }
+
+        if (mouseInput.y != 0)
+        {
+            // Rotate the camera horizontally
+            transform.Rotate(0f, mouseInput.y * Time.deltaTime * horizontalMouseSpeed, 0f);
+        }
+
+        if (mouseInput.z != 0)
+        {
+            // Zoom the camera
+            DistanceOffset = Mathf.Clamp(DistanceOffset + mouseInput.z * mouseScrollSpeed * Time.deltaTime, minDistance, maxDistance);
+            Camera.main.transform.localPosition = new Vector3(0f, CameraHeight, DistanceOffset);
+        }
+    }
+}
--- Assets/player_controller_rigidbody.cs
+++ Assets/player_controller_rigidbody.cs
@@ -4,44 +4,127 @@ using UnityEngine;
 
 public class player_controller_rigidbody : MonoBehaviour
 {
-    private string moveInputAxis = "Vertical";
-    private string turnInputAxis = "Horizontal";
-	
-	public float rotationRate = 360;
-	
-	public float moveSpeed = 10;
-	
-	private Rigidbody rb;
-	
-	private void Start()
+    public string moveY = "Vertical";
+    public string moveX = "Horizontal";
+
+    public Vector3 velocity;
+    
+    public float targetMoveSpeed = 10;
+    public float friction = 0.9f;
+    public float MoveSpeed { get; private set; }
+    public bool Grounded { get; private set; }
+
+    public float Speed;
+
+    private Rigidbody rb;
+
+    private void Start()
 	{
 		rb = GetComponent<Rigidbody>();
+        velocity = Vector3.zero;
+        Grounded = false;
 	}
 
     // Update is called once per frame
     void Update()
     {
-        float moveAxis = Input.GetAxis(moveInputAxis);
-        float turnAxis = Input.GetAxis(turnInputAxis);
-		
-		ApplyInput(moveAxis,turnAxis);
+        // Get character movement input from the player
+        Vector3 moveInput = new Vector3(Input.GetAxis(moveX), 0f, Input.GetAxis(moveY));
+        
+        // Ignore if input is incredibly small
+        if (Mathf.Abs(moveInput.x) < 0.001)
+        {
+            moveInput.x = 0;
+        }
+        if (Mathf.Abs(moveInput.z) < 0.001)
+        {
+            moveInput.z = 0;
+        }
+        
+        // Get direction of camera
+        Vector3 facing = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up).normalized;
+        Vector3 siding = Vector3.Cross(facing, Vector3.up);
+
+        // Reorient movement input vector relative to camera 
+        moveInput = -siding * moveInput.x + facing * moveInput.z;
+
+        //Handle Movement Input
+        ApplyInput(moveInput);
+
+        // Log current speed for debug use. Comment out or remove before release
+        Speed = velocity.magnitude;
+
+        // Apply velocity
+		rb.MovePosition(transform.position + velocity);
     }
 	
-	private void ApplyInput(float moveInput,
-	                        float turnInput)
-	{
-			Move(moveInput);	
-			Turn(turnInput);	
+	private void ApplyInput(Vector3 input)
+    {
+        // Handle Turning
+        Turn(input);
+
+        // Handle Moving
+        Move(input);	
 	}
 	
-	private void Move(float input)
-	{
-		//transform.Translate(Vector3.forward * input * moveSpeed);
-		rb.AddForce(transform.forward * input * moveSpeed, ForceMode.Force);
+	private void Move(Vector3 input)
+    {
+        // Sync movespeed to Time.deltaTime
+        MoveSpeed = targetMoveSpeed * (1 - friction * Time.deltaTime);
+        if (Grounded)
+        {
+            if (input != Vector3.zero)
+            {
+                // Movement speed is slower the greater the difference is between the movement input vector's magnitude
+                    //  and it's magnitude in the direction of the player's forward facing vector
+                float forwardMagnitude = Mathf.Max(Vector3.Dot(transform.forward, input) / input.magnitude, 0);
+
+                // Add movement to velocity
+                velocity += transform.forward.normalized * MoveSpeed * Mathf.Min(forwardMagnitude, 1);
+            }
+
+            // Stop if moving incredibly slowly
+            if (Mathf.Abs(velocity.magnitude) < 0.001)
+            {
+                velocity = Vector3.zero;
+            }
+            else
+            {
+                // Apply friction
+                velocity *= friction * Time.deltaTime;
+
+                // Apply extra friction if the movement vector is pointing behind the player.
+                // Friction is greater the greater the smaller the difference is between the movement input vector's magnitude
+                    // and the magnitude of the player's forward facing vector
+                if (Mathf.Abs(Vector3.Angle(transform.forward, input)) > 90)
+                {
+                    velocity *= friction * Time.deltaTime * Mathf.Max(Vector3.Dot(-transform.forward, input) / input.magnitude, 0);
+                }
+            }
+        }
 	}
-	
-	private void Turn(float input)
-	{
-		transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
+
+    // Because we use Slerp, it's faster to turn 90 degrees with a 135 degree Right-Down input
+        // turn than turning 90 degrees with a 90 degree Right input.
+        // This creates a tradeoff between turning speed and friction magnitude. 
+    private void Turn(Vector3 input)
+    {
+        if (input != Vector3.zero)
+        {
+            Vector3 target;
+            if (Vector3.Angle(transform.forward, input) == 180)
+            {
+                // Turn right 179 degrees if the angle between the forward facing vector and the movement input vector is 180 degrees
+                target = Vector3.Slerp(transform.forward, Vector3.Slerp(input, transform.right, 0.005f), Time.deltaTime);
+            }
+            else
+            { 
+                // Turn towards the movement input vector.
+                target = Vector3.Slerp(transform.forward, input, Time.deltaTime);
+            }
+            Quaternion look = Quaternion.LookRotation(target, Vector3.up);
+
+            rb.MoveRotation(look);
+        }
 	}
 }
